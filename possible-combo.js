a943a3df7fdefaef808279a1ad51e0500015253d
hitesht072 <hitesh.gahlot3@gmail.com>
2021-07-03 08:41:57 +0530

    Create possible-combo.js

---
+++ possible-combo.js
@@ -0,0 +1,40 @@
+var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  var alphabetMapping = {};
+  for (var i = 0; i < alphabet.length; i++) {
+    alphabetMapping[i + 1] = alphabet[i];
+  }
+
+var resultsCombination = [];
+function combination(encodeMsg) {
+	if (encodeMsg.length == 1) {
+		return 1;
+	} else {
+		var res = [];
+		res[0] = 1;
+		
+		for (var n = 1; n < encodeMsg.length; n++) {
+		  findComb(encodeMsg, "");
+			var condition = (parseInt(encodeMsg.slice(-2)) <= 26) ? 1 : 0;
+			res[n] = res[n-1] + condition;
+		}
+		return res[encodeMsg.length - 1];
+	}
+}
+
+function findComb(s, current) {
+    if (s.length == 0) {
+      resultsCombination.push(current);
+      return;
+    }
+
+    Object.entries(alphabetMapping).forEach(([key, val]) => {
+      if (s.startsWith(key)) {
+        findComb(s.substring(key.length), current + val, resultsCombination);
+      }
+    });
+}
+
+
+console.log('Total Combinations: ' + combination('12'));
+console.log('Combinations is/are: ');console.log(resultsCombination);
